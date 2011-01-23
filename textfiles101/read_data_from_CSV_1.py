35080e7dcb1a2a051376bd05a0bf1f4f18d146c0
Serdar <zstumgoren@zst.(none)>
2011-01-23 14:09:09 -0500

    textfiles101: adding some data points and the first basic sample script

---
+++ textfiles101/read_data_from_CSV_1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+"""
+Below is a bare-bones example showing how to read data from a CSV.
+
+We combine a "for" loop with the "open" function to read each line
+and print it to the command-line screen.
+ 
+The "open" function accepts a number of extra options, but in 
+in its most basic form can simply be called with the path to a file.
+"""
+
+# Note to Windows users: When specifying a file path, be sure you
+# set the file path appropriately with back-slashes
+for line in open('data/SmokeFreeComplaints_tab_delimited.csv'):
+    print line
